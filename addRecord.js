ce1c30475048e0599dcb5847bff37a04da154813
igt-higashi <n.higashi@imagitech.io>
2025-10-07 11:47:31 +0900

    PoC作成

---
+++ addRecord.js
@@ -0,0 +1,56 @@
+
+
+/**
+ * Kintone REST API を使用して、指定したアプリに新しいレコードを追加します。
+ * 
+ * このスクリプトは、Kintone REST API の `/k/v1/record.json` エンドポイントにPOST リクエストを送信し、
+ * 指定されたフィールド値を持つレコードを作成します。
+ * 
+ * ルックアップフィールドを登録する際には、ルックアップ元アプリのAPIトークンも必要です。
+ * 参考URL: https://cybozu.dev/ja/kintone/docs/rest-api/overview/authentication/#api-token
+ */
+
+const axios = require('axios');
+const config = require('./config.json');
+const { SUBDOMAIN, APP_ID_PIC, API_TOKEN_PIC, API_TOKEN_CUSTOMER } = config;
+// SUBDOMAIN: Kintoneのサブドメイン
+// APP_ID_PIC: 担当者管理アプリのID
+// const API_TOKEN_PIC: 担当者管理アプリのAPIトークン
+// const API_TOKEN_CUSTOMER: 顧客管理アプリ（ルックアップ元）のAPIトークン
+
+(async () => {
+  try {
+    const response = await axios.post(
+      `https://${SUBDOMAIN}.cybozu.com/k/v1/record.json`,
+      {
+        app: APP_ID_PIC,
+        record: {
+          // ルックアップキーとなるフィールドに値を設定
+          // 顧客No.などのコピーされるフィールドは自動でセットされる
+          顧客名: { value: '株式会社サイボウズ商事' },
+          姓: { value: '山田' },
+          名: { value: '太郎' },
+          姓_フリガナ: { value: 'ヤマダ' },
+          名_フリガナ: { value: 'タロウ' },
+          部署: { value: '営業部' },
+          役職: { value: '部長' },
+          電話番号: { value: '03-1234-5678' },
+          携帯番号: { value: '090-1234-5678' },
+          メールアドレス: { value: 'test@example.com' },
+          決裁権: { value: 'あり' },
+          // 備考はMULTI_LINE_TEXT
+          備考: { value: '備考行1\n備考行2\n備考行3' },
+        }
+      },
+      {
+        headers: {
+          'X-Cybozu-API-Token': `${API_TOKEN_PIC},${API_TOKEN_CUSTOMER}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    console.log('登録成功:', response.data);
+  } catch (err) {
+    console.error('登録失敗:', err.response?.data || err.message);
+  }
+})();
---
+++ getRecords.js
@@ -0,0 +1,30 @@
+/**
+ * Kintone REST API を使用して 指定したアプリからレコードを取得します。
+ * 
+ * このスクリプトは Kintone REST API の `/k/v1/records.json` エンドポイントにGET リクエストを送信し、
+ * アプリ内のレコードを JSON 形式で取得します。
+ */
+
+const axios = require('axios');
+const config = require('./config.json');
+const { SUBDOMAIN, APP_ID_PIC, API_TOKEN_PIC } = config;
+
+(async () => {
+  try {
+    const response = await axios.get(
+      `https://${SUBDOMAIN}.cybozu.com/k/v1/records.json`,
+      {
+        // 5件だけ取得するクエリ
+        // params: { app: APP_ID, query: 'limit 5' },
+        // 全件取得（最大500件まで）
+        params: { app: APP_ID_PIC, },
+        headers: {
+          'X-Cybozu-API-Token': API_TOKEN_PIC
+        }
+      }
+    );
+    console.log('取得成功:', response.data.records);
+  } catch (err) {
+    console.error('エラー:', err.response?.data || err.message);
+  }
+})();
